a00ad8073806c6568bd807fe0218af6e08754832
mede8 <andrei.medesan8@gmail.com>
2026-04-30 13:10:33 +0200

    scaled the data and saved to folder

--- utils/transform_data.py
+++ utils/transform_data.py
@@ -1,13 +1,19 @@
 import scipy.io
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 
 data = scipy.io.loadmat('data/Xtrain.mat')
 
-print(data.keys())
+# print(data.keys())
 
 array_data = data['Xtrain']
 df = pd.DataFrame(array_data)
-df.to_csv('xtrain.csv', index=False)
+df.to_csv('data/xtrain.csv', index=False)
 
-print(data)
+scaler = MinMaxScaler()
+scaled_data = scaler.fit_transform(df)
+scaled_df = pd.DataFrame(scaled_data, columns=df.columns)
+scaled_df.to_csv('data/xtrain_scaled.csv', index=False)
+
+# print(data)
